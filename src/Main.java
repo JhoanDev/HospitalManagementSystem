5126d5b61b0851c62b2a438302c9314f43273dfe
August0n3 <august0n3211@gmail.com>
2024-04-04 12:07:16 -0300

    tentando configurar a conexao com o banco

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 package src;
+import src.data.Banco;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de Gerenciamento de Hospital");
+        Banco bd = new Banco();
+        bd.querry_insup("INSERT INTO Paciente(cpf,telefone,data_nascimento,sexo,internado,plano_saude) VALUE (213821,18234701,2002-08-06,1,0,1)");
+        bd.desconect();
     }
 }
--- src/data/Banco.java
+++ src/data/Banco.java
@@ -1,5 +1,56 @@
 package src.data;
 
-public class Banco {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Banco { //nao funciona ainda
+    public Connection db = null;
+    public Statement statement = null;
     
+    public Banco(){
+        try{
+            this.db = DriverManager.getConnection("jdbc:sqlite:data.db");
+            this.statement = db.createStatement();
+            statement.setQueryTimeout(30);  // Espera só por 30 segundos para conectar     
+        }catch (SQLException e){
+            System.out.println("Erro na conexão");
+            System.out.println(e);
+        }
+    }
+
+    public void desconect(){
+        try {
+            if(db != null){
+              db.close();
+            }
+          } catch(SQLException e) {
+            System.out.println("Erro na hora de fechar conexão");
+            System.out.println(e);
+          }
+    }
+
+
+    public void querry_insup(String querry){
+        try{
+            statement.executeUpdate(querry);
+        }catch(SQLException e){
+            System.out.println("Erro na querry");
+            System.out.println(e);
+        }
+    }
+
+    public ResultSet querry_busca(String querry_busca){
+        ResultSet rs = null;
+        try{
+            rs = statement.executeQuery(querry_busca);
+            return rs;
+        }catch (SQLException e){
+            System.out.println("Erro na querry");
+            System.out.println(e);
+            return rs;
+        }
+    }
 }
