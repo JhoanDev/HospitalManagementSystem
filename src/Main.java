a950a96c40ee25066b159c9f76a98db3d81b12b8
August0n3 <August0n3211@gmail.com>
2024-04-08 19:03:45 -0300

    Arrumando banco e dependencias

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de Gerenciamento de Hospital");
         Banco bd = new Banco();
-        bd.querry_insup("INSERT INTO Paciente(cpf,telefone,data_nascimento,sexo,internado,plano_saude) VALUE (213821,18234701,2002-08-06,1,0,1)");
+        System.out.println(bd);
+        bd.querry_insup("INSERT INTO Paciente(cpf,telefone,data_nascimento,sexo,internado,plano_saude) VALUES (213821,18234701,2002-08-06,1,0,1)");
         bd.desconect();
     }
 }
