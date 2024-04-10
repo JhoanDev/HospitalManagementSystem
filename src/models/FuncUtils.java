c5906f144b7746e4950211e02a4e0d56b077e6d5
JhoanDev <jhoandev.log@gmail.com>
2024-04-10 20:41:12 -0300

    feat: Tratativas e imputs

--- src/models/FuncUtils.java
+++ src/models/FuncUtils.java
@@ -3,8 +3,11 @@ package src.models;
 import java.io.IOException;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class FuncUtils {
+    public static Scanner input = new Scanner(System.in);
 
     public static void clearScreen() {
         String os = System.getProperty("os.name").toLowerCase();
@@ -32,5 +35,60 @@ public class FuncUtils {
         // Convertendo a data em uma string formatada
         String strDate = formatter.format(date);
         return strDate;
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                System.out.println("Digite um número inteiro:");
+                return input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, digite um número inteiro válido.");
+                input.nextLine(); // Limpa o buffer do scanner
+            }
+        }
+    }
+
+    public static String readOnlyLettersAndSpaces() {
+        while (true) {
+            System.out.println("Digite uma string:");
+            String entrada = input.nextLine().trim(); // Remover espaços em branco no início e no final
+
+            // Verificar se a entrada é vazia
+            if (entrada.isEmpty()) {
+                System.out.println("Por favor, digite algo.");
+                continue;
+            }
+
+            // Verificar se a entrada contém apenas letras e espaços
+            if (entrada.matches("[a-zA-Z ]+")) {
+                return entrada;
+            } else {
+                System.out.println("Por favor, digite apenas letras e espaços.");
+            }
+        }
+    }
+
+    public static String readCPF() {
+        while (true) {
+            System.out.println("Digite um CPF (apenas números):");
+            String cpf = input.nextLine().trim();
+
+            // Remover caracteres de pontuação do CPF, se houver
+            cpf = cpf.replaceAll("[^0-9]", "");
+
+            // Verificar se o CPF tem 11 dígitos
+            if (cpf.length() != 11) {
+                System.out.println("CPF inválido. Por favor, digite exatamente 11 números.");
+                continue;
+            }
+
+            // Verificar se o CPF contém apenas dígitos
+            if (cpf.matches("[0-9]+")) {
+                return cpf;
+            } else {
+                System.out.println("CPF inválido. Por favor, digite apenas números.");
+            }
+        }
     }
 }
