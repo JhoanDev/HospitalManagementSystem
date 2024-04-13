a53180c1c1e495e914eee84d014f8e6f800fbfa8
JhoanDev <jhoandev.log@gmail.com>
2024-04-12 21:57:01 -0300

    Commit bobo

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ package src;
 
 import java.sql.SQLException;
 
-import src.data.Banco;
-
 public class Main {
-    public static Banco bd = new Banco();
+
 
     public static void main(String[] args) throws SQLException {
         
