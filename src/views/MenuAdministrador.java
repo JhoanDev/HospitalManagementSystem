06cd793e62b7f6aed6aac5d070b6a9fcab349c3a
JhoanDev <jhoandev.log@gmail.com>
2024-04-18 09:44:32 -0300

    Feat: view admin

--- src/views/MenuAdministrador.java
+++ src/views/MenuAdministrador.java
@@ -11,7 +11,7 @@ import src.utils.FuncUtils;
 public class MenuAdministrador {
     public static void adminMenu(Banco db) {
         int opcao = 0, opcao2 = 0;
-        String nome, cpf, telefone, login,senha;
+        String nome, cpf, telefone, login, senha;
         Date dataNasc, dataDeAdmissao;
         Time horarioDeTrabalhoInicio, horarioDeTrabalhoFinal;
         boolean sexo;
@@ -24,7 +24,7 @@ public class MenuAdministrador {
                 switch (opcao) {
                     case 1:
                         System.out.print("Digite o login: ");
-                        login = FuncUtils.readOnlyLettersAndSpaces();
+                        login = FuncUtils.readLogin();
 
                         System.out.print("Digite a senha: ");
                         senha = FuncUtils.readPassword();
@@ -50,23 +50,89 @@ public class MenuAdministrador {
                         System.out.print("insira o final do expediente, ");
                         horarioDeTrabalhoFinal = FuncUtils.readTime();
 
-                        AdministradorDao.cadastrarAdministrador(new Administrador(nome, cpf, telefone, dataNasc, sexo, salario, dataDeAdmissao,
-                                horarioDeTrabalhoInicio, horarioDeTrabalhoFinal,login,senha), db);
+                        AdministradorDao.cadastrarAdministrador(
+                                new Administrador(nome, cpf, telefone, dataNasc, sexo, salario, dataDeAdmissao,
+                                        horarioDeTrabalhoInicio, horarioDeTrabalhoFinal, login, senha),
+                                db);
                         break;
                     case 2:
-                        // Editar administrador
-                        // Implementação da edição aqui
+                        System.out.print("Digite o login do adm: ");
+                        login = FuncUtils.readLogin();
+                        administrador = AdministradorDao.buscaAdministrador(login, db);
+                        if (administrador != null) {
+                            System.out.println(administrador);
+                            System.out.println("O que deseja editar?");
+                            System.out.println("[1] - Nome");
+                            System.out.println("[2] - CPF");
+                            System.out.println("[3] - Telefone");
+                            System.out.println("[4] - Data de nascimento");
+                            System.out.println("[5] - Sexo");
+                            System.out.println("[6] - Salário");
+                            System.out.println("[7] - Data de admissão");
+                            System.out.println("[8] - Horário de trabalho (início)");
+                            System.out.println("[9] - Horário de trabalho (final)");
+                            System.out.println("[10] - Cancelar");
+                            opcao2 = FuncUtils.readInt();
+                            switch (opcao2) {
+                                case 1:
+                                    System.out.print("Digite o novo nome: ");
+                                    administrador.setNome(FuncUtils.readOnlyLettersAndSpaces());
+                                    break;
+                                case 2:
+                                    administrador.setCpf(FuncUtils.readCPF());
+                                    break;
+                                case 3:
+                                    administrador.setTelefone(FuncUtils.readPhoneNumber());
+                                    break;
+                                case 4:
+                                    administrador.setDataNasc(FuncUtils.readDate());
+                                    break;
+                                case 5:
+                                    System.out.println("Insira o novo sexo do administrador:");
+                                    administrador.setSexo(FuncUtils.readSex());
+                                    break;
+                                case 6:
+                                    administrador.setSalario(FuncUtils.readSalary());
+                                    break;
+                                case 7:
+                                    System.out.print("Insira a nova data de admissão, ");
+                                    administrador.setDataDeAdmissao(FuncUtils.readDate());
+                                    break;
+                                case 8:
+                                    System.out.print("Insira o novo horário de trabalho (início), ");
+                                    administrador.setHorarioDeTrabalhoInicio(FuncUtils.readTime());
+                                    break;
+                                case 9:
+                                    System.out.print("Insira o novo horário de trabalho (final), ");
+                                    administrador.setHorarioDeTrabalhoFinal(FuncUtils.readTime());
+                                    break;
+                                case 10:
+                                    break;
+                                default:
+                                    System.out.println("Opção inválida.");
+                                    break;
+                            }
+                            if (opcao2 != 10) {
+                                AdministradorDao.editarAdministrador(administrador, db);
+                            }
+                        }
                         break;
                     case 3:
-                        // Excluir administrador
-                        // Implementação da exclusão aqui
+                        System.out.println("Digite o login do adm: ");
+                        login = FuncUtils.readLogin();
+                        administrador = AdministradorDao.buscaAdministrador(login, db);
+                        AdministradorDao.excluirAdministrador(administrador, db);
                         break;
                     case 4:
                         // Listar administradores
                         break;
                     case 5:
-                        // Buscar administrador
-                        // Implementação da busca aqui
+                        System.out.println("Digite o login do adm: ");
+                        login = FuncUtils.readLogin();
+                        administrador = AdministradorDao.buscaAdministrador(login, db);
+                        if (administrador != null) {
+                            System.out.println("\n" + administrador);
+                        }
                         break;
                     case 6:
                         System.out.println("Saindo...");
